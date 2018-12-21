b1545581cfbed35f393e4c0f1a15fee6260cc037
Renato <nato.rjf@gmail.com>
2018-12-21 02:35:47 +0000

    Initial project setup

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <AppMap />
+  </div>
+</template>
+
+<script>
+import AppMap from '@/components/AppMap.vue';
+
+export default {
+  name: 'app',
+  components: {
+    AppMap,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
---
+++ src/components/AppMap.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="map">
+    <h1>Initial setup</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppMap',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
